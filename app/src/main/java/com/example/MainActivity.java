3e27f5bac1b976450d4baba35c222db8fde35e69
githubjewel <csedu.jewel@gmail.com>
2016-06-07 10:07:45 -0700

    Full Version 2

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -2,12 +2,14 @@ package com.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Toast.makeText(getBaseContext(),"LayoutDemo",Toast.LENGTH_LONG).show();
+        setContentView(R.layout.linear_demo);
     }
 }
